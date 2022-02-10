ee566091014b91728589d1c49dc68b8b967c1373
Lucas de Lima Silva <lcslimas@gmail.com>
2022-02-10 03:23:52 -0300

    Refactor: Ajustando Header

--- src/main/js/Components/Header/index.js
+++ src/main/js/Components/Header/index.js
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     const imgStyle = {
-        maxWidth: '150px',
-        margin: '0 auto',
+        maxWidth: '120px',
       };
     return(
         <header id="header bg-info">
             <div className="container">
-                <div className="m-auto pt-2">
+                <div className="m-auto pt-2 row">
                     <Link to="/movies">
-                        <figure className="logo d-flex m-0 justify-content-center">
+                        <figure className="logo d-flex m-0 p-0 col-2">
                             <img style={imgStyle} src="https://image.flaticon.com/icons/png/512/2093/2093027.png" alt="BusterFlix" />
                         </figure>
                     </Link>
+                    <h1 className="text-center justify-content-center d-flex my-auto ml-2">BusterFlix</h1>
                 </div>
             </div>
         </header>
